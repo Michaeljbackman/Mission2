af4efe6c9ffbc918bb193ce6d5431de08a7a6b45
Michaeljbackman <153117265+Michaeljbackman@users.noreply.github.com>
2025-01-15 13:56:52 -0700

    Syncing

---
+++ Mission2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ask user how many rolls they would like
+// For the number of times they said:
+// Generate a random number 1-6, generate a second random number 1-6, add two numbers together, update the counter of each #
+// For each of the numbers:
+// Print the number
+// Calculate the % of the times it was rolled
+// Print one asterisk for each percent it was rolled
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+}
